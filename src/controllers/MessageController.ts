1571b5ba9826bce7885ea005acc91944b0fc5170
Jeremy Zongker <jeremy@trilitech.com>
2022-10-18 17:27:56 -0500

    Logging personId

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -9,68 +9,73 @@ import { DeliveryHelper } from "../helpers/DeliveryHelper";
 @controller("/messages")
 export class MessageController extends MessagingBaseController {
 
+  @httpPost("/send")
+  public async send(req: express.Request<{}, {}, Message[]>, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      const promises: Promise<Message>[] = [];
+      req.body.forEach((message: Message) => {
+        message.messageType = "message";
+        if (au?.id) {
+          message.userId = au.id;
+          message.displayName = au.firstName + " " + au.lastName;
+          message.personId = au.personId
+        }
 
-    @httpPost("/send")
-    public async send(req: express.Request<{}, {}, Message[]>, res: express.Response): Promise<any> {
-        return this.actionWrapperAnon(req, res, async () => {
-            const promises: Promise<Message>[] = [];
-            req.body.forEach((message: Message) => {
-                message.messageType = "message";
-                promises.push(this.repositories.message.save(message).then(async (m: Message) => {
-                    await DeliveryHelper.sendMessages({ churchId: m.churchId, conversationId: m.conversationId, action: "message", data: m });
-                    return m;
-                }));
-            });
-            return this.repositories.message.convertAllToModel(await Promise.all(promises));
-        });
-    }
+        promises.push(this.repositories.message.save(message).then(async (m: Message) => {
+          await DeliveryHelper.sendMessages({ churchId: m.churchId, conversationId: m.conversationId, action: "message", data: m });
+          return m;
+        }));
+      });
+      return this.repositories.message.convertAllToModel(await Promise.all(promises));
+    });
+  }
 
-    @httpPost("/setCallout")
-    public async setCallout(req: express.Request<{}, {}, Message>, res: express.Response): Promise<any> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.chat.host)) return this.json({}, 401);
-            else {
-                req.body.messageType = "callout";
-                const m = this.repositories.message.convertToModel(await this.repositories.message.save(req.body));
-                await DeliveryHelper.sendMessages({ churchId: m.churchId, conversationId: m.conversationId, action: "callout", data: m });
-                return m;
-            }
-        });
-    }
+  @httpPost("/setCallout")
+  public async setCallout(req: express.Request<{}, {}, Message>, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.chat.host)) return this.json({}, 401);
+      else {
+        req.body.messageType = "callout";
+        const m = this.repositories.message.convertToModel(await this.repositories.message.save(req.body));
+        await DeliveryHelper.sendMessages({ churchId: m.churchId, conversationId: m.conversationId, action: "callout", data: m });
+        return m;
+      }
+    });
+  }
 
-    @httpDelete("/:id")
-    public async delete(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.chat.host)) return this.json({}, 401);
-            else {
-                const m = await this.repositories.message.loadById(au.churchId, id);
-                if (m !== null) {
-                    await this.repositories.message.delete(au.churchId, id);
-                    await DeliveryHelper.sendMessages({ churchId: au.churchId, conversationId: m.conversationId, action: "deleteMessage", data: m.id });
-                    return m;
-                }
-            }
-        });
-    }
-
-
-    @httpGet("/catchup/:churchId/:conversationId")
-    public async catchup(@requestParam("churchId") churchId: string, @requestParam("conversationId") conversationId: string, req: express.Request<{}, {}, []>, res: express.Response): Promise<any> {
-        return this.actionWrapperAnon(req, res, async () => {
-            const messages: Message[] = await this.repositories.message.loadForConversation(churchId, conversationId);
-            return this.repositories.message.convertAllToModel(messages);
-        });
-    }
+  @httpDelete("/:id")
+  public async delete(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.chat.host)) return this.json({}, 401);
+      else {
+        const m = await this.repositories.message.loadById(au.churchId, id);
+        if (m !== null) {
+          await this.repositories.message.delete(au.churchId, id);
+          await DeliveryHelper.sendMessages({ churchId: au.churchId, conversationId: m.conversationId, action: "deleteMessage", data: m.id });
+          return m;
+        }
+      }
+    });
+  }
 
 
-    private async createPrayerConversation(connection: Connection) {
-        return await this.repositories.conversation.save({
-            title: connection.displayName + "Prayer Request",
-            churchId: connection.churchId,
-            contentId: connection.conversationId,
-            contentType: "prayerRequest",
-        });
-    }
+  @httpGet("/catchup/:churchId/:conversationId")
+  public async catchup(@requestParam("churchId") churchId: string, @requestParam("conversationId") conversationId: string, req: express.Request<{}, {}, []>, res: express.Response): Promise<any> {
+    return this.actionWrapperAnon(req, res, async () => {
+      const messages: Message[] = await this.repositories.message.loadForConversation(churchId, conversationId);
+      return this.repositories.message.convertAllToModel(messages);
+    });
+  }
+
+
+  private async createPrayerConversation(connection: Connection) {
+    return await this.repositories.conversation.save({
+      title: connection.displayName + "Prayer Request",
+      churchId: connection.churchId,
+      contentId: connection.conversationId,
+      contentType: "prayerRequest",
+    });
+  }
 
 
 
--- src/repositories/MessageRepository.ts
+++ src/repositories/MessageRepository.ts
@@ -4,46 +4,46 @@ import { UniqueIdHelper } from "../helpers";
 
 export class MessageRepository {
 
-    public loadById(churchId: string, id: string) {
-        return DB.queryOne("SELECT * FROM messages WHERE id=? AND churchId=?;", [id, churchId]);
-    }
+  public loadById(churchId: string, id: string) {
+    return DB.queryOne("SELECT * FROM messages WHERE id=? AND churchId=?;", [id, churchId]);
+  }
 
-    public loadForConversation(churchId: string, conversationId: string) {
-        return DB.query("SELECT * FROM messages WHERE churchId=? AND conversationId=? ORDER BY timeSent", [churchId, conversationId]);
-    }
+  public loadForConversation(churchId: string, conversationId: string) {
+    return DB.query("SELECT * FROM messages WHERE churchId=? AND conversationId=? ORDER BY timeSent", [churchId, conversationId]);
+  }
 
-    public delete(churchId: string, id: string) {
-        return DB.query("DELETE FROM messages WHERE id=? AND churchId=?;", [id, churchId]);
-    }
+  public delete(churchId: string, id: string) {
+    return DB.query("DELETE FROM messages WHERE id=? AND churchId=?;", [id, churchId]);
+  }
 
-    public save(message: Message) {
-        return message.id ? this.update(message) : this.create(message);
-    }
+  public save(message: Message) {
+    return message.id ? this.update(message) : this.create(message);
+  }
 
-    private async create(message: Message) {
-        message.id = UniqueIdHelper.shortId();
-        const sql = "INSERT INTO messages (id, churchId, conversationId, userId, displayName, timeSent, messageType, content) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?);";
-        const params = [message.id, message.churchId, message.conversationId, message.userId, message.displayName, message.messageType, message.content];
-        await DB.query(sql, params);
-        return message;
-    }
+  private async create(message: Message) {
+    message.id = UniqueIdHelper.shortId();
+    const sql = "INSERT INTO messages (id, churchId, conversationId, userId, personId, displayName, timeSent, messageType, content) VALUES (?, ?, ?, ?, ?, ?, NOW(), ?, ?);";
+    const params = [message.id, message.churchId, message.conversationId, message.userId, message.personId, message.displayName, message.messageType, message.content];
+    await DB.query(sql, params);
+    return message;
+  }
 
-    private async update(message: Message) {
-        const sql = "UPDATE messages SET userId=?, displayName=?, content=? WHERE id=? AND churchId=?;";
-        const params = [message.userId, message.displayName, message.content, message.id, message.churchId]
-        await DB.query(sql, params)
-        return message;
-    }
+  private async update(message: Message) {
+    const sql = "UPDATE messages SET userId=?, displayName=?, content=? WHERE id=? AND churchId=?;";
+    const params = [message.userId, message.displayName, message.content, message.id, message.churchId]
+    await DB.query(sql, params)
+    return message;
+  }
 
-    public convertToModel(data: any) {
-        const result: Message = { id: data.id, churchId: data.churchId, conversationId: data.conversationId, userId: data.userId, displayName: data.displayName, timeSent: data.timeSent, messageType: data.messageType, content: data.content };
-        return result;
-    }
+  public convertToModel(data: any) {
+    const result: Message = { id: data.id, churchId: data.churchId, conversationId: data.conversationId, userId: data.userId, displayName: data.displayName, timeSent: data.timeSent, messageType: data.messageType, content: data.content };
+    return result;
+  }
 
-    public convertAllToModel(data: any[]) {
-        const result: Message[] = [];
-        data.forEach(d => result.push(this.convertToModel(d)));
-        return result;
-    }
+  public convertAllToModel(data: any[]) {
+    const result: Message[] = [];
+    data.forEach(d => result.push(this.convertToModel(d)));
+    return result;
+  }
 
 }
