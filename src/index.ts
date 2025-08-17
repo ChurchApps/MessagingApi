2b36dd01ae5fb41d096fef1745f84a71ebedbe1e
Jeremy Zongker <jeremy@zongker.net>
2025-08-16 22:30:20 -0500

    Fixing deployment error

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port = process.env.SERVER_PORT;
 Environment.init(process.env.APP_ENV).then(() => {
   Pool.initPool();
 
-  init().then(app => {
+  init().then((app: any) => {
     app.listen(port, () => {
       // Removed server startup logging to reduce CloudWatch noise
       // Only essential operational logs should go to CloudWatch in production
